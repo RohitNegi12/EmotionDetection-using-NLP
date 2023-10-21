d285944132f35f832ddf290802842337df093280
mihir1125 <mihir.laxmi@gmail.com>
2023-10-21 22:31:55 +0530

    Add pages to GUI, ignore venv of GUI and (pycache correctly)

---
+++ Emotion Detection GUI/pages/1_Word Cloud.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Hiding Streamlit watermarks
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+"Word Cloud"
